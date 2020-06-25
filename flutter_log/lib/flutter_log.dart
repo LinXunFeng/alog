4789d8dc47abe55f76c0891d74d3e185013ad1d8
LinXunFeng <598600855@qq.com>
2020-06-26 00:28:06 +0800

    完善注释

--- flutter_log/lib/flutter_log.dart
+++ flutter_log/lib/flutter_log.dart
@@ -12,13 +12,17 @@ String FLog(dynamic msg, { FLogMode mode = FLogMode.debug }) {
   if (kReleaseMode) { // release模式不打印
     return "";
   }
-  var chain = Chain.forTrace(StackTrace.current);
+  var chain = Chain.current(); // Chain.forTrace(StackTrace.current);
+  // 将 core 和 flutter 包的堆栈合起来（即相关数据只剩其中一条）
   chain = chain.foldFrames((frame) => frame.isCore || frame.package == "flutter");
+  // 取出所有信息帧
   final frames = chain.toTrace().frames;
+  // 找到当前函数的信息帧
   final idx = frames.indexWhere((element) => element.member == "FLog");
   if (idx == -1 || idx+1 >= frames.length) {
     return "";
   }
+  // 调用当前函数的函数信息帧
   final frame = frames[idx+1];
 
   var modeStr = "";
